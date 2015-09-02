f9c25708e21e301cb7a9c6254bfb2634624b79e1
zenparsing <zenparsing@gmail.com>
2015-09-02 09:57:54 -0400

    Use es-observable-tests package.

--- test/default.js
+++ test/default.js
@@ -1,4 +1,4 @@
-var runTests = require("./observable-tests.js").runTests;
+var runTests = require("es-observable-tests.js").runTests;
 var Observable = require("../zen-observable.js").Observable;
 
 runTests(Observable);
