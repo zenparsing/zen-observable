36e775d412236bab2cacfc9248c6111b3ae237ba
Kevin Smith <KevinH.Smith@coxinc.com>
2018-01-28 13:55:17 -0500

    Add constructor tests

--- test/of.js
+++ test/of.js
@@ -1,6 +1,17 @@
 const Observable = require('../src/Observable');
 const assert = require('assert');
+const { testMethodProperty } = require('./properties');
 
 describe('of', () => {
+  it('is a method on Observable', () => {
+    testMethodProperty(Observable, 'of', {
+      configurable: true,
+      writable: true,
+      length: 0,
+    });
+  });
 
+  it('uses the this value if it is a function', () => {
+
+  });
 });
