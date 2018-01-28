70af672d3b6a8176c93d7b56d2999c3b8a90e6b2
Kevin Smith <KevinH.Smith@coxinc.com>
2018-01-28 14:17:59 -0500

    Add tests for observer.closed and Observable.of

--- test/observer-closed.js
+++ test/observer-closed.js
@@ -1,6 +1,38 @@
 const Observable = require('../src/Observable');
 const assert = require('assert');
+const { testMethodProperty } = require('./properties');
 
 describe('observer.closed', () => {
+  it('is a getter on SubscriptionObserver.prototype', () => {
+    let observer;
+    new Observable(x => { observer = x }).subscribe();
+    testMethodProperty(Object.getPrototypeOf(observer), 'closed', {
+      get: true,
+      configurable: true,
+      writable: true,
+      length: 1
+    });
+  });
 
+  it('returns false when the subscription is open', () => {
+    new Observable(observer => {
+      assert.equal(observer.closed, false);
+    }).subscribe();
+  });
+
+  it('returns true when the subscription is completed', async () => {
+    let observer;
+    new Observable(x => { observer = x; }).subscribe();
+    await null;
+    observer.complete();
+    assert.equal(observer.closed, true);
+  });
+
+  it('returns true when the subscription is errored', async () => {
+    let observer;
+    new Observable(x => { observer = x; }).subscribe(null, () => {});
+    await null;
+    observer.error();
+    assert.equal(observer.closed, true);
+  });
 });
