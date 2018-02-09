8375eb4c20cc5d2d745a80e1cad489b829be51ea
Kevin Smith <KevinH.Smith@coxinc.com>
2018-02-09 12:01:59 -0500

    Simplify parse testing function

--- test/extras/parse.js
+++ test/extras/parse.js
@@ -1,13 +1,11 @@
 export function parse(string) {
-  return new Observable(observer => {
-    (async () => {
+  return new Observable(async observer => {
+    await null;
+    for (let char of string) {
+      if (observer.closed) return;
+      else if (char !== '-') observer.next(char);
       await null;
-      for (let char of string) {
-        if (observer.closed) return;
-        else if (char !== '-') observer.next(char);
-        await null;
-      }
-      observer.complete();
-    })();
+    }
+    observer.complete();
   });
 }
